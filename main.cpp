e51e0f87199e6886ec90f97d8c4e40c31efc9d12
Abhaya Nigam <61977981+abhayanigam@users.noreply.github.com>
2021-10-01 07:01:50 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <stdio.h> 
+#define p printf(
+#define f )
+#define s scanf(
+#define printf("\n") m
+
 int main() 
 
 {
    int x, y; 
-   printf("Enter Value of x ");
-   scanf("%d", &x); 
-   printf("\nEnter Value of y "); 
-   scanf("%d", &y);
+   p "Enter Value of x " f;
+   s "%d", &x f; 
+   p "\nEnter Value of y " f; 
+   s "%d", &y f;
 
 
     int temp = x; 
      x = y;
      y = temp; 
-     printf("\nAfter Swapping: x = %d, y = %d", x, y);
-   
+     p "After Swapping: x = %d, y = %d", x, y f;
+   m;
    return 0;
 
   }
